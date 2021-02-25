d48e4089950c690e015d5e959b9d557fc97314c7
Balázs Dukai <balazs.dukai@gmail.com>
2021-02-25 20:08:21 +0100

    This is some commit
    And a very long commit messsage lormadfisdf
    sdfasdfuuasda
    fdsfasdkkkkkkkkkkkkkkkkkkkkkkkkkkkddddddddddddddddddd
    hhhhhhhhhhhhhhhhhhhhhhhhhhhhdsfasf
    (test-tile-fdhzr23529)

--- hello.cpp
+++ hello.cpp
@@ -4,11 +4,6 @@
 
 #include <iostream>
 
-// do some change here
-//some long commit
-
-// and lets do a new tag
-
 int main () {
   int version(6);
   std::cout << "Test version: " << version << std::endl;
